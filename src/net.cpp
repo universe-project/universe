bbd8fe082a2ebd16de52140d331eb3a13aa7153a
Universe <admin@unicoin.pw>
2017-04-16 09:12:24 +0400

    Delete other node

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"walletbuilders.com", "node.walletbuilders.com"},
+    {"unicoin.pw", "unicoin.pw"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
