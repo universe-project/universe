a891f67579de8a78b2f07d7545fb3784ae6de0be
Universe <admin@unicoin.pw>
2017-04-17 10:01:13 +0400

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"unicoin.pw", "unicoin.pw"},
+    {"unicoin.pw", "188.190.156.232"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
